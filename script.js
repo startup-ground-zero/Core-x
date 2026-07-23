b25b5d209b1681ecd0daf0468ffa176daa744832
initiation <kyrkats@hotmail.com>
2026-07-23 21:25:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
             slides[currentSlide].classList.remove('active');
             currentSlide = (currentSlide + 1) % slides.length;
             slides[currentSlide].classList.add('active');
-        }, 5000);
+        }, 3000);
     }
 
     // --- Scroll Reveal ---
